d03b95a0d166aebdff8b40f75a473850b4abf6cc
caojianyi <caojianyi@dxy.cn>
2020-11-22 22:27:54 +0800

    springboot aop切面使用学习

---
+++ src/main/java/com/example/springboot/aop/AopLog.java
@@ -0,0 +1,74 @@
+package com.example.springboot.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author caojianyi@dxy.cn
+ * @date 2020-11-22 21:41
+ * @Description springboot aop切面日志添加(切面=切点+通知)
+ */
+@Component
+@Slf4j
+@Aspect
+public class AopLog {
+
+    //aop切面多线程临时变量存放
+    public ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    //定义切点 excution(方法修饰类型 返回类型 方法名 参数  异常模式) 用于切面的连接点
+    @Pointcut("execution(public * com.example.springboot..*.*(..))")
+    public void aopWebLog() {
+
+    }
+
+    /**
+     * 前置通知->进入controller方法之前处理
+     *
+     * @param joinPoint
+     */
+    @Before("aopWebLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        startTime.set(System.currentTimeMillis());
+
+        //接收到请求,记录请求内容(请求上下文)
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest  request=attributes.getRequest();
+
+        //记录下请求内容
+        log.info("URL"+request.getRequestURI().toString());
+        log.info("Http方法:"+request.getMethod());
+        log.info("IP地址:"+request.getRemoteHost());
+        log.info("类的方法:"+joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
+        log.info("参数:"+request.getQueryString());
+    }
+
+
+    /**
+     * 代码执行完成后 通知
+     * @param reObject
+     */
+    @AfterReturning(pointcut = "aopWebLog()",returning = "reObject")
+    public  void doAfterReturning(Object reObject){
+        //处理完请求,返回内容
+        log.info("返回值:"+reObject);
+        log.info("应答时间:"+(System.currentTimeMillis()-startTime.get()));
+    }
+
+
+    /**
+     * 代码抛出异常后 通知
+     * @param joinPoint
+     */
+    @AfterThrowing(pointcut = "aopWebLog()",throwing = "e")
+    public void addAfterThrowingLogger(JoinPoint joinPoint,Exception e){
+        log.error("执行"+"异常",e);
+
+    }
+}
